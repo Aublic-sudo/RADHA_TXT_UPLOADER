45c75c6c7cc603c64e82180b7694bd378d0316a8
Aublic-sudo <rstock937@gmail.com>
2026-03-04 13:57:29 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,23 +1,18 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
-# Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "27607585"))
-API_HASH = environ.get("API_HASH", "725f60b51e233703576199334610011a")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8495221315:AAF9Q6qXOYiaKWQExCQU1U6Qwry75hF0jqI")
+API_ID = int(environ.get("API_ID"))
+API_HASH = environ.get("API_HASH")
+BOT_TOKEN = environ.get("BOT_TOKEN")
 
-OWNER = int(environ.get("OWNER", "5666930893"))
-CREDIT = environ.get("CREDIT", "KANHA 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER"))
+CREDIT = environ.get("CREDIT")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5666930893').split(',')
+TOTAL_USER = os.environ.get("TOTAL_USERS").split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5666930893').split(',')
+AUTH_USER = os.environ.get("AUTH_USERS").split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
-  
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
-
